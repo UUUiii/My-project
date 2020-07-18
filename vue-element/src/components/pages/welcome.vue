64ba14769461433e3a01e178d0d6ba5d100f0d28
UUUiii <1040506922@qq.com>
2020-07-18 18:53:15 +0800

    wx项目

--- vue-element/src/components/pages/welcome.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="photo">
-           <img class="backgound-photo" src="/static/img/background1.jpg" width="90%" height="800px">
-    </div>
-</template>
-<style scoped>
-
-</style>
